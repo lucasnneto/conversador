4dca0a1b88e8e94de01f8b36713ed5a800c5d7c6
Lucas Neto <lucasn-oliveira@hotmail.com>
2022-05-25 11:43:56 -0300

    278d615

---
+++ js/about-legacy.c5ca48ef.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkconversador"]=self["webpackChunkconversador"]||[]).push([[443],{9030:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an about page")])])}],u=t(1001),a={},c=(0,u.Z)(a,r,s,!1,null,null,null),l=c.exports}}]);
+//# sourceMappingURL=about-legacy.c5ca48ef.js.map
